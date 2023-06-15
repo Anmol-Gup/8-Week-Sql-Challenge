05c50b3c876d31e622b7fb6ec098122eded3ef25
Surya Prakash Gupta <66009201+Anmol-Gup@users.noreply.github.com>
2023-06-15 22:20:48 +0530

    Update Danny's Dinner.sql

--- Danny's Dinner.sql
+++ Danny's Dinner.sql
@@ -1,4 +1,5 @@
 --Case Study #1 - Danny's Dinner
+--https://8weeksqlchallenge.com/case-study-1/
 -- 1. What is the total amount each customer spent at the restaurant?
 SELECT customer_id,SUM(price) AS Amount
 FROM dannys_diner.sales s JOIN dannys_diner.menu m 
