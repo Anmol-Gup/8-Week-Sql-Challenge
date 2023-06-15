8ec1ccfeeb6c737acd38da317c3a48252022b2bc
Surya Prakash Gupta <66009201+Anmol-Gup@users.noreply.github.com>
2023-06-15 23:15:30 +0530

    Update Danny's Dinner.sql
    Completed question 3

--- Danny's Dinner.sql
+++ Danny's Dinner.sql
@@ -9,3 +9,15 @@ ON m.product_id=s.product_id
 SELECT customer_id,COUNT(join_date) AS num_days_visited
 FROM dannys_diner.members
 GROUP BY customer_id;
+
+-- 3. What was the first item from the menu purchased by each customer?
+WITH first_item
+AS
+(
+  SELECT customer_id,s.product_id,product_name,order_date,ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date,s.product_id) AS rno 
+  FROM dannys_diner.sales s
+  JOIN dannys_diner.menu m
+  ON m.product_id=s.product_id
+) 
+SELECT customer_id,product_name,order_date FROM first_item 
+WHERE rno=1;
