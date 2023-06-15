8c04cd6223ec7d88a320e37a4c393d1cfdcddbf4
Surya Prakash Gupta <66009201+Anmol-Gup@users.noreply.github.com>
2023-06-15 22:31:02 +0530

    Update Danny's Dinner.sql
    Completed 2nd question

--- Danny's Dinner.sql
+++ Danny's Dinner.sql
@@ -4,3 +4,8 @@
 SELECT customer_id,SUM(price) AS Amount
 FROM dannys_diner.sales s JOIN dannys_diner.menu m 
 ON m.product_id=s.product_id
+
+-- 2. How many days has each customer visited the restaurant?
+SELECT customer_id,COUNT(join_date) AS num_days_visited
+FROM dannys_diner.members
+GROUP BY customer_id;
